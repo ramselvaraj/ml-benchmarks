82309b45667474ec0b210a57409c6212ce968e06
ramselvaraj <ramselvaraj01@gmail.com>
2026-04-06 11:33:49 -0700

    added data-analysis folder

---
+++ _data-analysis-scripts/dataAnalysis.py
@@ -0,0 +1,5 @@
+# post inference data analysis code will go here
+'''
+ - ideally each model will have a result.csv which is a standardized format we will define
+ - we can then write standarized scripts to load and plot everything we need
+'''
